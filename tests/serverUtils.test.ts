047f5975054b94407a792d9e5f9fea4db547c82e
Nicolas <138610419+naikath@users.noreply.github.com>
2023-11-05 11:39:37 -0300

    test(lang-utils): Add test to refactor data storage

--- tests/serverUtils.test.ts
+++ tests/serverUtils.test.ts
@@ -5,15 +5,22 @@ test('generated array', () => {
 	expect(Array.isArray(data)).toBeTruthy();
 });
 
-test('contains data', () => {
-	expect(data).toContainEqual({
+test('contains data items', () => {
+	const joinedArray = [...data[0].items, ...data[1].items];
+	expect(joinedArray).toContainEqual({
 		id: 101,
 		img: '/icons/web/html.svg',
 		name: 'HTML',
 	});
-	expect(data).toContainEqual({
+
+	expect(joinedArray).toContainEqual({
 		id: 201,
 		img: '/icons/js/nodejs.svg',
 		name: 'NodeJS',
 	});
+});
+
+test('is well structured', () => {
+	expect(data[0]).toHaveProperty('group');
+	expect(data[0]).toHaveProperty('items');
 });
